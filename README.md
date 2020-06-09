56ead7ed7900ad6e12266d25a7d63d8685c14b18
hhr-hit <15684580826@163.com>
2020-06-09 12:49:10 +0800

    0609

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# SpringBoot-Neo4j-KBQA
+﻿# SpringBoot-Neo4j-KBQA
  SpringBoot整合Neo4j开发KBQA自动问答
+路径和数据库下载后自己配置
+字典文件也是
--- createNeo4j.bat
+++
@@ -1,5 +0,0 @@
-call neo4j stop
-call rmdir /s/q D:\neo4j-community-3.5.5\data\databases\graph.db
-call neo4j-admin import --mode csv --database graph.db --nodes  "D:\_HhrWorkSpace\Python\neo4j\entity.csv" --relationships "D:\_HhrWorkSpace\Python\neo4j\relationship.csv"
-call neo4j start
-pause
